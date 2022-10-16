e09f4140f03457f439f1b01875124d6d4f225631
Kara Ballew <karakeatley@gmail.com>
2022-10-16 10:10:38 -0400

    updated test data

--- model.py
+++ model.py
@@ -121,9 +121,9 @@ class Home_Resources(db.Model):
                         autoincrement=True,
                         primary_key=True)
     community_id = db.Column(db.Integer, db.ForeignKey('communities.community_id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     home_resource_name = db.Column(db.String(64), nullable=False)
-    home_resource_link = db.Column(db.String(64), nullable=False)
+    home_resource_link = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
         return f"<Home Resource name={self.home_resource_name}>"
@@ -138,7 +138,7 @@ class Community_Boards(db.Model):
                           autoincrement=True,
                           primary_key=True)
     community_id = db.Column(db.Integer, db.ForeignKey('communities.community_id'), nullable=False)
-    community_board_title = db.Column(db.String(32), nullable=False)
+    community_board_title = db.Column(db.String(64), nullable=False)
 
     def __repr__(self):
         return f"<Community Board title={self.community_board_title}>"
@@ -192,7 +192,7 @@ class State_Region_Resources(db.Model):
     state_region_id = db.Column(db.Integer, db.ForeignKey('state_regions.state_region_id'), nullable=False)
     admin_access_id = db.Column(db.Integer, db.ForeignKey('admin_access.admin_access_id'), nullable=False)
     state_region_resource_name = db.Column(db.String(64), nullable=False)
-    state_region_resource_link = db.Column(db.String(64), nullable=False)
+    state_region_resource_link = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):            
          return f"<State & Region Resources state_region_resource_id={self.state_region_resource_id} \
@@ -210,7 +210,7 @@ class National_Resources(db.Model):
     nation_id = db.Column(db.Integer, db.ForeignKey('nations.nation_id'), nullable=False)
     admin_access_id = db.Column(db.Integer, db.ForeignKey('admin_access.admin_access_id'), nullable=False)
     national_resource_name = db.Column(db.String(64), nullable=False)
-    national_resource_link = db.Column(db.String(64), nullable=False)
+    national_resource_link = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):      
         return f"<National Resources national_resource_id={self.national_resource_id}\
